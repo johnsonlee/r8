0a9b61108fb2922ced3744adbf4e1b8e3924cdc8
Ian Zerny <zerny@google.com>
2020-02-04 11:42:48 +0100

    Version 2.0.27
    Cherry-pick: Remove $deserializeLambda$ from all classes.
    CL: https://r8-review.googlesource.com/48301
    Bug: 148734239
    Change-Id: Iab8787cf610b0241e9ebd2a851211073da59160c

--- src/main/java/com/android/tools/r8/ir/desugar/LambdaRewriter.java
+++ src/main/java/com/android/tools/r8/ir/desugar/LambdaRewriter.java
@@ -228,6 +228,7 @@ public class LambdaRewriter {
 
   /** Remove lambda deserialization methods. */
   public boolean removeLambdaDeserializationMethods(Iterable<DexProgramClass> classes) {
+    boolean anyRemoved = false;
     for (DexProgramClass clazz : classes) {
       // Search for a lambda deserialization method and remove it if found.
       List<DexEncodedMethod> directMethods = clazz.directMethods();
@@ -240,14 +241,15 @@ public class LambdaRewriter {
             assert encoded.accessFlags.isStatic();
             assert encoded.accessFlags.isSynthetic();
             clazz.removeDirectMethod(i);
+            anyRemoved = true;
 
             // We assume there is only one such method in the class.
-            return true;
+            break;
           }
         }
       }
     }
-    return false;
+    return anyRemoved;
   }
 
   /** Adjust accessibility of referenced application symbols or creates necessary accessors. */
