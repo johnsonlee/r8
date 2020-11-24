0f2cc7aee92f08fc586415435a89315b60f7a6b6
Morten Krogh-Jespersen <mkroghj@google.com>
2020-11-24 13:38:22 +0100

    Check for accessibility weaking before rebinding in devirtualizer
    Bug: 173812804
    Change-Id: Ifc3924fb330c281f4c4fedd87dd34c7831b9ccd4

--- src/main/java/com/android/tools/r8/ir/optimize/Devirtualizer.java
+++ src/main/java/com/android/tools/r8/ir/optimize/Devirtualizer.java
@@ -311,7 +311,11 @@ public class Devirtualizer {
     if (newResolutionResult == null
         || newResolutionResult
             .isAccessibleForVirtualDispatchFrom(context, appView.appInfo())
-            .isPossiblyFalse()) {
+            .isPossiblyFalse()
+        || !newResolutionResult
+            .getResolvedMethod()
+            .getAccessFlags()
+            .isAtLeastAsVisibleAs(resolutionResult.getResolvedMethod().getAccessFlags())) {
       return target;
     }
 
