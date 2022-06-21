ca37e6c25fcd32df549bf72f619e6f58deb905b3
Christoffer Quist Adamsen <christofferqa@google.com>
2022-06-21 13:18:01 +0200

    Fix inadequate rewriting of enum array types after enum unboxing
    Bug: b/236618700
    Change-Id: I53b65574a82d3c84b0591ab00c2552a089c5e1ec

--- src/main/java/com/android/tools/r8/ir/optimize/enums/EnumUnboxerImpl.java
+++ src/main/java/com/android/tools/r8/ir/optimize/enums/EnumUnboxerImpl.java
@@ -371,7 +371,7 @@ public class EnumUnboxerImpl extends EnumUnboxer {
   private void analyzeFieldInstruction(
       FieldInstruction fieldInstruction, Set<DexType> eligibleEnums, ProgramMethod context) {
     DexField field = fieldInstruction.getField();
-    DexProgramClass enumClass = getEnumUnboxingCandidateOrNull(field.holder);
+    DexProgramClass enumClass = getEnumUnboxingCandidateOrNull(field.getHolderType());
     if (enumClass != null) {
       FieldResolutionResult resolutionResult = appView.appInfo().resolveField(field, context);
       if (resolutionResult.isSuccessfulResolution()) {
@@ -537,14 +537,15 @@ public class EnumUnboxerImpl extends EnumUnboxer {
           }
         }
       } else if (use.isFieldPut()) {
-        DexType type = use.asFieldInstruction().getField().type;
-        if (enumUnboxingCandidatesInfo.isCandidate(type)) {
-          eligibleEnums.add(type);
+        DexProgramClass enumClass =
+            getEnumUnboxingCandidateOrNull(use.asFieldInstruction().getField().getType());
+        if (enumClass != null) {
+          eligibleEnums.add(enumClass.getType());
         }
       } else if (use.isReturn()) {
-        DexType returnType = code.method().getReference().proto.returnType;
-        if (enumUnboxingCandidatesInfo.isCandidate(returnType)) {
-          eligibleEnums.add(returnType);
+        DexProgramClass enumClass = getEnumUnboxingCandidateOrNull(code.context().getReturnType());
+        if (enumClass != null) {
+          eligibleEnums.add(enumClass.getType());
         }
       }
     }
