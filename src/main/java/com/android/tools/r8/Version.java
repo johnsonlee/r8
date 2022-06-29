84f7d463437a40f82331256d06f21dc50fadb908
Søren Gjesse <sgjesse@google.com>
2022-06-29 12:48:27 +0200

    Version 3.3.69
    Bug: b/235325369
    Change-Id: Ib80e22b512f7fa795e6749df11d5a23914eb7907

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -11,7 +11,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "3.3.68";
+  public static final String LABEL = "3.3.69";
 
   private Version() {
   }
