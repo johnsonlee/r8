e1d6e7856f18eec3b40c35942172dad170ab050f
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-09-30 17:26:38 +0000

    Adding tarfile member sanitization to extractall()

--- tools/utils.py
+++ tools/utils.py
@@ -393,7 +393,26 @@ def unpack_archive(filename):
     shutil.rmtree(dest_dir)
   dirname = os.path.dirname(os.path.abspath(filename))
   with tarfile.open(filename, 'r:gz') as tar:
-    tar.extractall(path=dirname)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner) 
+        
+    
+    safe_extract(tar, path=dirname)
 
 def check_gcert():
   subprocess.check_call(['gcert'])
