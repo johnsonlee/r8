dd3a0cccf67bbb143f4ecb55efc35a190a4f4a68
Christoffer Quist Adamsen <christofferqa@google.com>
2023-12-13 14:13:09 +0100

    Add reproduction of incorrect instance-get canonicalization
    Bug: b/315877832
    Change-Id: I26a84ab7f6224e39e00c8d6fa532ec9462a00c0b

---
+++ src/test/java/com/android/tools/r8/ir/optimize/canonicalization/EffectivelyFinalInstanceFieldCanonicalizationAfterConstructorInliningTest.java
@@ -0,0 +1,76 @@
+// Copyright (c) 2023, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.ir.optimize.canonicalization;
+
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.NoRedundantFieldLoadElimination;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class EffectivelyFinalInstanceFieldCanonicalizationAfterConstructorInliningTest
+    extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void test() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(getClass())
+        .addKeepMainRule(Main.class)
+        .enableInliningAnnotations()
+        .enableNoRedundantFieldLoadEliminationAnnotations()
+        .setMinApi(parameters)
+        .run(parameters.getRuntime(), Main.class)
+        // TODO(b/315877832): Fix canonicalization.
+        .assertSuccessWithOutputLines(
+            parameters.canInitNewInstanceUsingSuperclassConstructor()
+                ? "null, world!"
+                : "Hello, world!");
+  }
+
+  static class Main {
+
+    public static void main(String[] args) {
+      Object alwaysNull = System.currentTimeMillis() > 0 ? null : new Object();
+      A a = new A(alwaysNull);
+      System.out.println(a);
+    }
+  }
+
+  static class A {
+
+    @NoRedundantFieldLoadElimination Object f;
+
+    A(Object o) {
+      f = o;
+      if (f == null) {
+        f = "Hello";
+      }
+      print(f);
+    }
+
+    @NeverInline
+    static void print(Object o) {
+      System.out.print(o);
+    }
+
+    @Override
+    public String toString() {
+      return ", world!";
+    }
+  }
+}
