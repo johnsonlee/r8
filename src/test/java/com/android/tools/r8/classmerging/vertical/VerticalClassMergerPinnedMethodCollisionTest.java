ba6a9956162dff5fecb36ee31dee510de2a01b83
Christoffer Quist Adamsen <christofferqa@google.com>
2023-12-19 10:28:55 +0100

    Reproduce renaming of pinned method due to collision
    Bug: b/316862633
    Change-Id: I272d17d3fe90c96e57d9700fb6be722687dd0703

---
+++ src/test/java/com/android/tools/r8/classmerging/vertical/VerticalClassMergerPinnedMethodCollisionTest.java
@@ -0,0 +1,79 @@
+// Copyright (c) 2023, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.classmerging.vertical;
+
+import static com.android.tools.r8.utils.codeinspector.AssertUtils.assertFailsCompilation;
+
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class VerticalClassMergerPinnedMethodCollisionTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void test() throws Exception {
+    assertFailsCompilation(
+        () ->
+            testForR8(parameters.getBackend())
+                .addInnerClasses(getClass())
+                .addKeepMainRule(Main.class)
+                .addKeepRules(
+                    "-keep class " + UserSub.class.getTypeName() + " {",
+                    "  void f(" + B.class.getTypeName() + ");",
+                    "}")
+                .addVerticallyMergedClassesInspector(
+                    inspector -> inspector.assertMergedIntoSubtype(A.class))
+                .enableInliningAnnotations()
+                .setMinApi(parameters)
+                .compile());
+  }
+
+  static class Main {
+
+    public static void main(String[] args) {
+      new User().f(new B());
+      new UserSub().f(new B());
+    }
+  }
+
+  static class A {}
+
+  static class B extends A {
+
+    @Override
+    public String toString() {
+      return "B";
+    }
+  }
+
+  static class User {
+
+    @NeverInline
+    void f(A a) {
+      System.out.println(a);
+    }
+  }
+
+  static class UserSub extends User {
+
+    void f(B b) {
+      System.out.println(b);
+    }
+  }
+}
---
+++ src/test/java/com/android/tools/r8/classmerging/vertical/VerticalClassMergerPinnedMethodInterfaceCollisionTest.java
@@ -0,0 +1,93 @@
+// Copyright (c) 2023, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.classmerging.vertical;
+
+import static com.android.tools.r8.utils.codeinspector.AssertUtils.assertFailsCompilation;
+
+import com.android.tools.r8.NoUnusedInterfaceRemoval;
+import com.android.tools.r8.NoVerticalClassMerging;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class VerticalClassMergerPinnedMethodInterfaceCollisionTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void test() throws Exception {
+    assertFailsCompilation(
+        () ->
+            testForR8(parameters.getBackend())
+                .addInnerClasses(getClass())
+                .addKeepClassAndMembersRules(Main.class)
+                .addKeepRules(
+                    "-keep class " + OtherUser.class.getTypeName() + " {",
+                    "  void f(" + B.class.getTypeName() + ");",
+                    "}")
+                .addVerticallyMergedClassesInspector(
+                    inspector -> inspector.assertMergedIntoSubtype(A.class))
+                .enableInliningAnnotations()
+                .enableNoUnusedInterfaceRemovalAnnotations()
+                .enableNoVerticalClassMergingAnnotations()
+                .setMinApi(parameters)
+                .compile());
+  }
+
+  static class Main {
+
+    public static void main(String[] args) {
+      keep(new UserImpl());
+      new OtherUser().f(new B());
+    }
+
+    static void keep(User user) {
+      user.f(new B());
+    }
+  }
+
+  static class A {}
+
+  static class B extends A {
+
+    @Override
+    public String toString() {
+      return "B";
+    }
+  }
+
+  @NoUnusedInterfaceRemoval
+  @NoVerticalClassMerging
+  interface User {
+
+    void f(A a);
+  }
+
+  static class UserImpl implements User {
+
+    @Override
+    public void f(A a) {
+      System.out.println("UserImpl");
+    }
+  }
+
+  static class OtherUser {
+
+    void f(B b) { // f(B)
+      System.out.println(b);
+    }
+  }
+}
