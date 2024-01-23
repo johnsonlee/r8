3830ed4e730f55266840d8024e9c53c749b4f2ea
Christoffer Quist Adamsen <christofferqa@google.com>
2024-01-23 19:11:15 +0100

    Reproduce missing enum unboxing analysis of instance-of
    Bug: b/321689478
    Change-Id: Ib29e061065a9793e367a4ff3d7931d94353addf0

---
+++ src/test/java/com/android/tools/r8/enumunboxing/EnumUnboxingInstanceOfTest.java
@@ -0,0 +1,62 @@
+// Copyright (c) 2024, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.enumunboxing;
+
+import static com.android.tools.r8.utils.codeinspector.AssertUtils.assertFailsCompilation;
+
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class EnumUnboxingInstanceOfTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void test() throws Exception {
+    assertFailsCompilation(
+        () ->
+            testForR8(parameters.getBackend())
+                .addInnerClasses(getClass())
+                .addKeepMainRule(Main.class)
+                .enableInliningAnnotations()
+                .setMinApi(parameters)
+                .compile());
+  }
+
+  static class Main {
+
+    public static void main(String[] args) {
+      Object nullableObject = System.currentTimeMillis() > 0 ? new Object() : null;
+      System.out.println(test(nullableObject));
+
+      // Use enum instance so we don't treat MyEnum as uninstantiated.
+      MyEnum e = System.currentTimeMillis() > 0 ? MyEnum.A : MyEnum.B;
+      System.out.println(e.name());
+    }
+
+    @NeverInline
+    static boolean test(Object e) {
+      return e instanceof MyEnum;
+    }
+  }
+
+  enum MyEnum {
+    A,
+    B
+  }
+}
