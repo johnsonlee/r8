e8a026ca0d9647913dd20a67b5f292d7e68b8ae7
Ian Zerny <zerny@google.com>
2024-01-25 13:30:17 +0100

    [KeepAnno] Test that all generated files are up-to-date
    Bug: b/248408342
    Change-Id: I3045ea55ccf2ab93f9d7376f999ba83f3a47debf

--- src/test/java/com/android/tools/r8/keepanno/utils/KeepAnnoMarkdownGenerator.java
+++ src/test/java/com/android/tools/r8/keepanno/utils/KeepAnnoMarkdownGenerator.java
@@ -49,9 +49,9 @@ import java.util.Set;
 
 public class KeepAnnoMarkdownGenerator {
 
-  public static void generateMarkdownDoc(Generator generator) {
+  public static void generateMarkdownDoc(Generator generator, Path projectRoot) {
     try {
-      new KeepAnnoMarkdownGenerator(generator).internalGenerateMarkdownDoc();
+      new KeepAnnoMarkdownGenerator(generator).internalGenerateMarkdownDoc(projectRoot);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
@@ -202,10 +202,11 @@ public class KeepAnnoMarkdownGenerator {
     generator.println(line);
   }
 
-  private void internalGenerateMarkdownDoc() throws IOException {
-    Path template = Paths.get("doc/keepanno-guide.template.md");
+  private void internalGenerateMarkdownDoc(Path projectRoot) throws IOException {
+    Path relativePath = Paths.get("doc", "keepanno-guide.template.md");
+    Path template = projectRoot.resolve(relativePath);
     println("[comment]: <> (DO NOT EDIT - GENERATED FILE)");
-    println("[comment]: <> (Changes should be made in " + template + ")");
+    println("[comment]: <> (Changes should be made in " + relativePath + ")");
     println();
     List<String> readAllLines = FileUtils.readAllLines(template);
     TableEntry root = new TableEntry(0, "root", "root", null);
