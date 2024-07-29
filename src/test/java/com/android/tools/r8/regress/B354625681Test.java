efb63734ed9dd0b695eb8110053a61a1bb1f6567
Clément Béra <clementbera@google.com>
2024-07-29 10:55:19 +0200

    Add reproduction for Verification error
    Bug: b/354625681
    Change-Id: Ib0b22004854647fc3f132a38b8169825de75ef30

---
+++ src/test/java/com/android/tools/r8/regress/B354625681Test.java
@@ -0,0 +1,86 @@
+// Copyright (c) 2024, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.regress;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.utils.AndroidApiLevel;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class B354625681Test extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  private static final List<String> EXPECTED_OUTPUT = ImmutableList.of("over");
+
+  @Test
+  public void testD8AndJvm() throws Exception {
+    testForRuntime(parameters)
+        .addInnerClasses(getClass())
+        .run(parameters.getRuntime(), TestClass.class)
+        .assertSuccessWithOutputLines(EXPECTED_OUTPUT);
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    if (parameters.isCfRuntime()
+        || (parameters.isDexRuntime()
+            && parameters.getApiLevel().isGreaterThanOrEqualTo(AndroidApiLevel.N))) {
+      testForR8(parameters.getBackend())
+          .addInnerClasses(getClass())
+          .addKeepMainRule(TestClass.class)
+          .setMinApi(parameters)
+          .run(parameters.getRuntime(), TestClass.class)
+          .assertSuccessWithOutputLines(EXPECTED_OUTPUT);
+    } else {
+      Assert.assertThrows(
+          Throwable.class,
+          () ->
+              testForR8(parameters.getBackend())
+                  .addInnerClasses(getClass())
+                  .addKeepMainRule(TestClass.class)
+                  .setMinApi(parameters)
+                  .run(parameters.getRuntime(), TestClass.class)
+                  .assertSuccessWithOutputLines(EXPECTED_OUTPUT));
+    }
+  }
+
+  static class TestClass {
+
+    void mainTest() {
+      int i2 = 171;
+      String s3 = "sssssssssssssssssssss";
+      String s2 = "s";
+      String[][] ss = {{"s"}};
+      for (int i = -6; i < 6; i += 1) {
+        s2 = s2 + s3;
+        try {
+          if (ss[i2] == null) {}
+        } catch (Throwable e) {
+        }
+      }
+    }
+
+    public static void main(String[] strArr) {
+      TestClass _instance = new TestClass();
+      _instance.mainTest();
+      System.out.println("over");
+    }
+  }
+}
