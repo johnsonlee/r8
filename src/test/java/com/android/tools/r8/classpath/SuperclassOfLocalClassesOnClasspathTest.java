a6485701bdf18f774a6ea2db01380b31a8332a91
Søren Gjesse <sgjesse@google.com>
2024-08-08 12:39:59 +0200

    Add test of local classes with enclosing class/methods on classpath
    Bug: b/358281941
    Change-Id: Ifacafa2d7565f11900cb3ddb7797e39eb38a822a

---
+++ src/test/java/com/android/tools/r8/classpath/SuperclassOfLocalClassesOnClasspathTest.java
@@ -0,0 +1,130 @@
+// Copyright (c) 2024, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.package com.android.tools.r8.classpath;
+
+package com.android.tools.r8.classpath;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.utils.AndroidApiLevel;
+import com.android.tools.r8.utils.codeinspector.AssertUtils;
+import java.nio.file.Path;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class SuperclassOfLocalClassesOnClasspathTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  static Path classpath;
+
+  @BeforeClass
+  public static void setUpClasspath() throws Exception {
+    // Build classpath DEX with API level 1 to work with all API levels.
+    classpath = getStaticTemp().newFile("classpath.zip").toPath();
+    testForD8(getStaticTemp())
+        .addProgramClasses(A.class)
+        .setMinApi(AndroidApiLevel.B)
+        .compile()
+        .writeToZip(classpath);
+  }
+
+  @Test
+  public void testD8() throws Exception {
+    parameters.assumeDexRuntime();
+    testForD8()
+        .addClasspathClasses(A.class)
+        .addInnerClasses(A.class)
+        .addProgramClasses(I.class)
+        .addProgramClasses(TestClass.class)
+        .setMinApi(parameters)
+        .addRunClasspathFiles(classpath)
+        .run(parameters.getRuntime(), TestClass.class)
+        .assertSuccess();
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    AssertUtils.assertFailsCompilation(
+        () ->
+            testForR8(parameters.getBackend())
+                // Split A and its inner classes on classpath/program.
+                .addClasspathClasses(A.class)
+                .addInnerClasses(A.class)
+                .addProgramClasses(I.class)
+                .addProgramClasses(TestClass.class)
+                .addKeepMainRule(TestClass.class)
+                .setMinApi(parameters)
+                .addRunClasspathFiles(classpath)
+                .run(parameters.getRuntime(), TestClass.class)
+                .assertSuccess());
+  }
+
+  interface I {
+    void m();
+  }
+
+  static class A {
+    static {
+      // Anonymous local class in <clinit>.
+      new I() {
+        public void m() {}
+      };
+    }
+
+    static {
+      // Named local class in <clinit>.
+      class Local {}
+      new Local();
+    }
+
+    public A() {
+      anonymousLocalClass();
+      namedLocalClass();
+
+      // Anonymous local class in <init>.
+      new I() {
+        public void m() {}
+      };
+
+      // Named local class in <init>.
+      class Local {
+        public void m() {}
+      }
+
+      new Local();
+    }
+
+    private void anonymousLocalClass() {
+      new I() {
+        public void m() {}
+      };
+    }
+
+    private void namedLocalClass() {
+      class Local {
+        public void m() {}
+      }
+      new Local();
+    }
+  }
+
+  static class TestClass {
+
+    public static void main(String[] args) {
+      new A();
+    }
+  }
+}
