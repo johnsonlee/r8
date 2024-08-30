baf1c0247fda4b66aecd9b39a2ac91e80c946d1a
Søren Gjesse <sgjesse@google.com>
2024-08-30 11:16:00 +0200

    Update Kotlin 2.0.20 test expectation
    Change-Id: I443f5b51323fb47b525b0f9afcc5aacbfd68aaf9
    Bug: B/362425784

--- src/test/java/com/android/tools/r8/kotlin/KotlinClassInlinerTest.java
+++ src/test/java/com/android/tools/r8/kotlin/KotlinClassInlinerTest.java
@@ -8,6 +8,7 @@ import static com.android.tools.r8.KotlinCompilerTool.KotlinCompilerVersion.KOTL
 import static com.android.tools.r8.KotlinCompilerTool.KotlinCompilerVersion.KOTLINC_1_5_0;
 import static com.android.tools.r8.KotlinCompilerTool.KotlinCompilerVersion.KOTLINC_1_6_0;
 import static com.android.tools.r8.KotlinCompilerTool.KotlinCompilerVersion.KOTLINC_1_9_21;
+import static com.android.tools.r8.KotlinCompilerTool.KotlinCompilerVersion.KOTLINC_2_0_20;
 import static com.android.tools.r8.KotlinCompilerTool.KotlinCompilerVersion.KOTLIN_DEV;
 import static com.android.tools.r8.utils.codeinspector.Matchers.isAbsent;
 import static com.android.tools.r8.utils.codeinspector.Matchers.isAbsentIf;
@@ -213,7 +214,7 @@ public class KotlinClassInlinerTest extends AbstractR8KotlinTestBase {
                                     kotlinParameters
                                         .getCompiler()
                                         .getCompilerVersion()
-                                        .isEqualTo(KOTLINC_1_9_21),
+                                        .isBetweenBothIncluded(KOTLINC_1_9_21, KOTLINC_2_0_20),
                                     i ->
                                         i.assertIsCompleteMergeGroup(
                                             SyntheticItemsTestUtils.syntheticLambdaClass(mainKt, 0),
