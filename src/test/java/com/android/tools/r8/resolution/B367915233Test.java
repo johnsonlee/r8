4857658d90f7793a88c3bf1de42e360368f3faee
Søren Gjesse <sgjesse@google.com>
2024-09-19 09:19:22 +0200

    Add reproduction of VerifyError calling java.lang.Object.clone()
    The VerifyError only happens on OpenJDK, both Dalvik and ART accept
    calling the protected method java.lang.Object.clone() outside a
    subclass.
    
    Bug: b/367915233
    Change-Id: Iac28749d8ae8322b53617d565b095a9f349678f2

---
+++ src/test/java/com/android/tools/r8/resolution/B367915233Test.java
@@ -0,0 +1,50 @@
+// Copyright (c) 2024, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.resolution;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.utils.StringUtils;
+import java.util.LinkedHashMap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class B367915233Test extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  private static final String EXPECTED_OUTPUT = StringUtils.lines("Hello, world!");
+
+  @Test
+  public void testR8() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(getClass())
+        .addKeepMainRule(TestClass.class)
+        .setMinApi(parameters)
+        .run(parameters.getRuntime(), TestClass.class)
+        .applyIf(
+            parameters.isCfRuntime(),
+            r -> r.assertFailureWithErrorThatThrows(VerifyError.class),
+            r -> r.assertSuccessWithOutput(EXPECTED_OUTPUT));
+  }
+
+  static class TestClass {
+
+    public static void main(String[] args) {
+      new LinkedHashMap<>().clone();
+      System.out.println("Hello, world!");
+    }
+  }
+}
