f9453cf6ca21c0e9df0543f169b01fc7a375ecd3
Søren Gjesse <sgjesse@google.com>
2024-09-30 11:31:32 +0200

    Add reproduction of issue 370303498
    Bug: b/370303498
    Change-Id: I4b8ef699bb6d698171c737843e61fccda91fd20c

---
+++ src/test/java/com/android/tools/r8/ir/optimize/B370303498Test.java
@@ -0,0 +1,67 @@
+// Copyright (c) 2024, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.ir.optimize;
+
+import com.android.tools.r8.SingleTestRunResult;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class B370303498Test extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void testJvm() throws Exception {
+    parameters.assumeJvmTestParameters();
+    testForJvm(parameters)
+        .addInnerClasses(getClass())
+        .run(parameters.getRuntime(), TestClass.class)
+        .assertFailureWithErrorThatThrows(ArrayIndexOutOfBoundsException.class);
+  }
+
+  @Test
+  public void testD8() throws Exception {
+    parameters.assumeDexRuntime();
+    testForD8(parameters.getBackend())
+        .addInnerClasses(getClass())
+        .setMinApi(parameters)
+        .run(parameters.getRuntime(), TestClass.class)
+        // TODO(b/370303498): Should throw ArrayIndexOutOfBoundsException.
+        .assertSuccess();
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(getClass())
+        .addKeepMainRule(TestClass.class)
+        .setMinApi(parameters)
+        .run(parameters.getRuntime(), TestClass.class)
+        .applyIf(
+            parameters.isCfRuntime(),
+            r -> r.assertFailureWithErrorThatThrows(ArrayIndexOutOfBoundsException.class),
+            // TODO(b/370303498): Should throw ArrayIndexOutOfBoundsException.
+            SingleTestRunResult::assertSuccess);
+  }
+
+  static class TestClass {
+
+    public static void main(String[] args) {
+      java.lang.reflect.Array.set(new int[2], 2, new Object());
+    }
+  }
+}
