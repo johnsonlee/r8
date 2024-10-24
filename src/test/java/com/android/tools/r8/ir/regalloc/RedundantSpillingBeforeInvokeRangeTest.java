c546acea1328e3248e16165264047115f42bcd76
Christoffer Adamsen <christofferqa@google.com>
2024-10-24 14:59:37 +0200

    Reproduce many redundant moves for invoke-range
    Bug: b/302281605
    Change-Id: I20dbdd21c565e8e38d5f2cf39b8b83181a7d0aeb

---
+++ src/test/java/com/android/tools/r8/ir/regalloc/RedundantSpillingBeforeInvokeRangeTest.java
@@ -0,0 +1,75 @@
+// Copyright (c) 2024, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.ir.regalloc;
+
+import static com.android.tools.r8.utils.codeinspector.Matchers.isPresent;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import com.android.tools.r8.utils.codeinspector.InstructionSubject;
+import com.android.tools.r8.utils.codeinspector.MethodSubject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class RedundantSpillingBeforeInvokeRangeTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withDefaultDexRuntime().withMaximumApiLevel().build();
+  }
+
+  @Test
+  public void testD8() throws Exception {
+    testForD8()
+        .addInnerClasses(getClass())
+        .addOptionsModification(
+            options -> options.getTestingOptions().enableRegisterAllocation8BitRefinement = true)
+        .release()
+        .setMinApi(parameters)
+        .compile()
+        .inspect(
+            inspector -> {
+              MethodSubject testMethodSubject =
+                  inspector.clazz(Main.class).uniqueMethodWithOriginalName("test");
+              assertThat(testMethodSubject, isPresent());
+              // TODO(b/302281605): Should not naively move each argument into different low
+              //  registers.
+              assertEquals(
+                  18,
+                  testMethodSubject
+                      .streamInstructions()
+                      .filter(InstructionSubject::isMove)
+                      .count());
+            });
+  }
+
+  static class Main {
+
+    Main(long a, long b, long c, long d, long e, long f, long g, long h) {}
+
+    void test(long a, long b, long c, long d, long e, long f, long g, long h) {
+      forceIntoLowRegister(a, a);
+      forceIntoLowRegister(b, b);
+      forceIntoLowRegister(c, c);
+      forceIntoLowRegister(d, d);
+      forceIntoLowRegister(e, e);
+      forceIntoLowRegister(f, f);
+      forceIntoLowRegister(g, g);
+      forceIntoLowRegister(h, h);
+      new Main(a, b, c, d, e, f, g, h);
+    }
+
+    static void forceIntoLowRegister(long a, long b) {}
+  }
+}
