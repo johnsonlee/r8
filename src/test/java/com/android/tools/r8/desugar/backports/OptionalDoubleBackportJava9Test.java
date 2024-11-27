0ff3415ebe3965d6ced3759dcf263f12fa2d0ec7
Rico Wind <ricow@google.com>
2024-11-27 15:41:17 +0000

    Revert "Move Java9 backport test to java 9 module"
    Revert submission 98720
    
    Reason for revert: breaking bots
    
    Reverted changes: /q/submissionid:98720
    
    Change-Id: Ib7154ad59bbc936568a57dcc807298182cb4cd3a

--- src/test/java/com/android/tools/r8/desugar/backports/OptionalDoubleBackportJava9Test.java
+++ src/test/java/com/android/tools/r8/desugar/backports/OptionalDoubleBackportJava9Test.java
@@ -2,14 +2,17 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-package backport;
+package com.android.tools.r8.desugar.backports;
 
-import com.android.tools.r8.TestBase;
+import static com.android.tools.r8.utils.FileUtils.JAR_EXTENSION;
+
 import com.android.tools.r8.TestParameters;
 import com.android.tools.r8.TestRuntime.CfVm;
+import com.android.tools.r8.ToolHelper;
 import com.android.tools.r8.ToolHelper.DexVm.Version;
-import com.android.tools.r8.desugar.backports.AbstractBackportTest;
 import com.android.tools.r8.utils.AndroidApiLevel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.OptionalDouble;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -19,7 +22,7 @@ import org.junit.runners.Parameterized.Parameters;
 public final class OptionalDoubleBackportJava9Test extends AbstractBackportTest {
   @Parameters(name = "{0}")
   public static Iterable<?> data() {
-    return TestBase.getTestParameters()
+    return getTestParameters()
         .withDexRuntimesStartingFromIncluding(Version.V7_0_0)
         .withApiLevelsStartingAtIncluding(AndroidApiLevel.N)
         .withCfRuntimesStartingFromIncluding(CfVm.JDK9)
@@ -27,8 +30,11 @@ public final class OptionalDoubleBackportJava9Test extends AbstractBackportTest 
         .build();
   }
 
+  private static final Path TEST_JAR =
+      Paths.get(ToolHelper.EXAMPLES_JAVA9_BUILD_DIR).resolve("backport" + JAR_EXTENSION);
+
   public OptionalDoubleBackportJava9Test(TestParameters parameters) {
-    super(parameters, OptionalDouble.class, OptionalDoubleBackportJava9Main.class);
+    super(parameters, OptionalDouble.class, TEST_JAR, "backport.OptionalDoubleBackportJava9Main");
     // Note: The methods in this test exist in android.jar from Android T. When R8 builds targeting
     // Java 11 move these tests to OptionalBackportTest (out of examplesJava9).
 
@@ -37,33 +43,5 @@ public final class OptionalDoubleBackportJava9Test extends AbstractBackportTest 
     ignoreInvokes("of");
 
     registerTarget(AndroidApiLevel.T, 4);
-  }
-
-  public static class OptionalDoubleBackportJava9Main {
-
-    public static void main(String[] args) {
-      testIfPresentOrElseDouble();
-      testStreamDouble();
-    }
-
-    private static void testIfPresentOrElseDouble() {
-      OptionalDouble value = OptionalDouble.of(1.0d);
-      OptionalDouble emptyValue = OptionalDouble.empty();
-      value.ifPresentOrElse(val -> {}, () -> assertTrue(false));
-      emptyValue.ifPresentOrElse(val -> assertTrue(false), () -> {});
-    }
-
-    private static void testStreamDouble() {
-      OptionalDouble value = OptionalDouble.of(2d);
-      OptionalDouble emptyValue = OptionalDouble.empty();
-      assertTrue(value.stream().count() == 1);
-      assertTrue(emptyValue.stream().count() == 0);
-    }
-
-    private static void assertTrue(boolean value) {
-      if (!value) {
-        throw new AssertionError("Expected <true> but was <false>");
-      }
-    }
   }
 }
--- src/test/java/com/android/tools/r8/desugar/backports/OptionalIntBackportJava9Test.java
+++ src/test/java/com/android/tools/r8/desugar/backports/OptionalIntBackportJava9Test.java
@@ -2,14 +2,17 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-package backport;
+package com.android.tools.r8.desugar.backports;
 
-import com.android.tools.r8.TestBase;
+import static com.android.tools.r8.utils.FileUtils.JAR_EXTENSION;
+
 import com.android.tools.r8.TestParameters;
 import com.android.tools.r8.TestRuntime.CfVm;
+import com.android.tools.r8.ToolHelper;
 import com.android.tools.r8.ToolHelper.DexVm.Version;
-import com.android.tools.r8.desugar.backports.AbstractBackportTest;
 import com.android.tools.r8.utils.AndroidApiLevel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.OptionalInt;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -19,7 +22,7 @@ import org.junit.runners.Parameterized.Parameters;
 public final class OptionalIntBackportJava9Test extends AbstractBackportTest {
   @Parameters(name = "{0}")
   public static Iterable<?> data() {
-    return TestBase.getTestParameters()
+    return getTestParameters()
         .withDexRuntimesStartingFromIncluding(Version.V7_0_0)
         .withApiLevelsStartingAtIncluding(AndroidApiLevel.N)
         .withCfRuntimesStartingFromIncluding(CfVm.JDK9)
@@ -27,8 +30,11 @@ public final class OptionalIntBackportJava9Test extends AbstractBackportTest {
         .build();
   }
 
+  private static final Path TEST_JAR =
+      Paths.get(ToolHelper.EXAMPLES_JAVA9_BUILD_DIR).resolve("backport" + JAR_EXTENSION);
+
   public OptionalIntBackportJava9Test(TestParameters parameters) {
-    super(parameters, OptionalInt.class, OptionalIntBackportJava9Main.class);
+    super(parameters, OptionalInt.class, TEST_JAR, "backport.OptionalIntBackportJava9Main");
     // Note: The methods in this test exist in android.jar from Android T. When R8 builds targeting
     // Java 11 move these tests to OptionalBackportTest (out of examplesJava9).
 
@@ -37,33 +43,5 @@ public final class OptionalIntBackportJava9Test extends AbstractBackportTest {
     ignoreInvokes("of");
 
     registerTarget(AndroidApiLevel.T, 4);
-  }
-
-  public static class OptionalIntBackportJava9Main {
-
-    public static void main(String[] args) {
-      testIfPresentOrElseInt();
-      testStreamInt();
-    }
-
-    private static void testIfPresentOrElseInt() {
-      OptionalInt value = OptionalInt.of(1);
-      OptionalInt emptyValue = OptionalInt.empty();
-      value.ifPresentOrElse(val -> {}, () -> assertTrue(false));
-      emptyValue.ifPresentOrElse(val -> assertTrue(false), () -> {});
-    }
-
-    private static void testStreamInt() {
-      OptionalInt value = OptionalInt.of(2);
-      OptionalInt emptyValue = OptionalInt.empty();
-      assertTrue(value.stream().count() == 1);
-      assertTrue(emptyValue.stream().count() == 0);
-    }
-
-    private static void assertTrue(boolean value) {
-      if (!value) {
-        throw new AssertionError("Expected <true> but was <false>");
-      }
-    }
   }
 }
