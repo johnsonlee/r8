55f0e8343405579157c814c2208aca0125178c0d
Christoffer Adamsen <christofferqa@google.com>
2025-01-17 12:14:13 +0100

    Move R8 partial resources test to partial subpackage
    This way all R8 partial tests can be run with the *partial* pattern.
    
    Change-Id: I90dac2ec92ab12c26a64386bed25031e11541536

--- src/test/java/com/android/tools/r8/androidresources/partial/ResourceShrinkingInPartialR8Test.java
+++ src/test/java/com/android/tools/r8/androidresources/partial/ResourceShrinkingInPartialR8Test.java
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, the R8 project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-package com.android.tools.r8.androidresources;
+package com.android.tools.r8.androidresources.partial;
 
 import com.android.tools.r8.R8PartialTestBuilder;
 import com.android.tools.r8.TestBase;
