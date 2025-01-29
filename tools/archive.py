b86997a2086593d7ac00b92e4c625bf12c3d0d31
Rico Wind <ricow@google.com>
2025-01-29 07:00:45 +0100

    Also upload keepanno annotations to latest dev
    Change-Id: I72002ed8354ec057ea850cc635cdd142cae3ac79

--- tools/archive.py
+++ tools/archive.py
@@ -333,7 +333,9 @@ def Run(options):
                           GetMavenUrl(is_main))
 
             # Upload R8LIB to latest on dev channel, this is used by godbolt.
-            if file == utils.R8LIB_JAR and version.endswith('-dev'):
+            if (version.endswith('-dev') and
+                (file == utils.R8LIB_JAR or
+                 file == utils.KEEPANNO_ANNOTATIONS_JAR)):
                 latest_dst = GetUploadDestination('latest-dev', file_name,
                                                   is_main)
                 print('Uploading %s to %s' % (tagged_jar, latest_dst))
