4fafc7f353413459fe5e26b7049cfed3b825abc1
Clément Béra <clementbera@google.com>
2025-03-11 11:18:38 +0100

    Move PathOf test to java 11 tests
    Change-Id: I6e70a6777d9e2f6aa0cd45c27e9e3fb7621d87d9

--- src/test/examplesJava11/path/PathOfTest.java
+++ src/test/examplesJava11/path/PathOfTest.java
@@ -2,20 +2,18 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-package com.android.tools.r8.desugar.desugaredlibrary.jdk11;
+package path;
 
 import static com.android.tools.r8.desugar.desugaredlibrary.test.CompilationSpecification.DEFAULT_SPECIFICATIONS;
 import static com.android.tools.r8.desugar.desugaredlibrary.test.LibraryDesugaringSpecification.JDK11_PATH;
 
 import com.android.tools.r8.TestParameters;
-import com.android.tools.r8.ToolHelper;
 import com.android.tools.r8.desugar.desugaredlibrary.DesugaredLibraryTestBase;
 import com.android.tools.r8.desugar.desugaredlibrary.test.CompilationSpecification;
 import com.android.tools.r8.desugar.desugaredlibrary.test.LibraryDesugaringSpecification;
 import com.android.tools.r8.utils.StringUtils;
 import com.google.common.collect.ImmutableList;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,10 +26,9 @@ public class PathOfTest extends DesugaredLibraryTestBase {
   private final TestParameters parameters;
   private final LibraryDesugaringSpecification libraryDesugaringSpecification;
   private final CompilationSpecification compilationSpecification;
-
-  private static final Path INPUT_JAR = Paths.get(ToolHelper.EXAMPLES_JAVA11_JAR_DIR + "path.jar");
+  ;
   private static final String EXPECTED_OUTPUT = StringUtils.lines("foo/bar", "bar");
-  private static final String MAIN_CLASS = "path.PathExample";
+  private static final Class<?> MAIN_CLASS = PathExample.class;
 
   @Parameters(name = "{0}, spec: {1}, {2}")
   public static List<Object[]> data() {
@@ -53,9 +50,18 @@ public class PathOfTest extends DesugaredLibraryTestBase {
   @Test
   public void test() throws Exception {
     testForDesugaredLibrary(parameters, libraryDesugaringSpecification, compilationSpecification)
-        .addProgramFiles(INPUT_JAR)
+        .addInnerClassesAndStrippedOuter(getClass())
         .addKeepMainRule(MAIN_CLASS)
         .run(parameters.getRuntime(), MAIN_CLASS)
         .assertSuccessWithOutput(EXPECTED_OUTPUT);
+  }
+
+  public static class PathExample {
+
+    public static void main(String[] args) {
+      Path thePath = Path.of("foo", "bar");
+      System.out.println(thePath);
+      System.out.println(Path.of(thePath.toUri()).getFileName());
+    }
   }
 }
