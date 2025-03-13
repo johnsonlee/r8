ee37c7bbe8d09b78e21b2d0fe87abbb3cfaf4c61
Clément Béra <clementbera@google.com>
2025-03-13 14:13:56 +0100

    Move pattern matching instanceof test to jdk17 module
    Change-Id: I19c7339022398a45b061d2f035f7f784d7c5f332

--- src/test/examplesJava17/pattern_matching_for_instanceof/PatternMatchingForInstanceOfTest.java
+++ src/test/examplesJava17/pattern_matching_for_instanceof/PatternMatchingForInstanceOfTest.java
@@ -2,16 +2,14 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-package com.android.tools.r8.java_language.pattern_matching_for_instenceof;
+package pattern_matching_for_instanceof;
 
 import com.android.tools.r8.R8TestBuilder;
 import com.android.tools.r8.TestBase;
 import com.android.tools.r8.TestParameters;
 import com.android.tools.r8.TestParametersCollection;
 import com.android.tools.r8.TestRuntime.CfVm;
-import com.android.tools.r8.examples.jdk17.PatternMatchingForInstanceof;
 import com.google.common.collect.ImmutableList;
-import java.nio.file.Path;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,14 +18,11 @@ import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
-public class PattternMatchingForInstanceOfTest extends TestBase {
+public class PatternMatchingForInstanceOfTest extends TestBase {
 
   @Parameter public TestParameters parameters;
 
   private static List<String> EXPECTED = ImmutableList.of("Hello, world!");
-
-  private static final Path JAR = PatternMatchingForInstanceof.jar();
-  private static final String MAIN = PatternMatchingForInstanceof.Main.typeName();
 
   @Parameters(name = "{0}")
   public static TestParametersCollection data() {
@@ -42,18 +37,18 @@ public class PattternMatchingForInstanceOfTest extends TestBase {
   public void testJvm() throws Exception {
     parameters.assumeJvmTestParameters();
     testForJvm(parameters)
-        .addRunClasspathFiles(JAR)
-        .run(parameters.getRuntime(), MAIN)
+        .addInnerClassesAndStrippedOuter(getClass())
+        .run(parameters.getRuntime(), Main.class)
         .assertSuccessWithOutputLines(EXPECTED);
   }
 
   @Test
   public void testD8() throws Exception {
     testForD8(parameters.getBackend())
-        .addProgramFiles(JAR)
+        .addInnerClassesAndStrippedOuter(getClass())
         .setMinApi(parameters)
         .compile()
-        .run(parameters.getRuntime(), MAIN)
+        .run(parameters.getRuntime(), Main.class)
         .assertSuccessWithOutputLines(EXPECTED);
   }
 
@@ -62,16 +57,25 @@ public class PattternMatchingForInstanceOfTest extends TestBase {
     parameters.assumeR8TestParameters();
     R8TestBuilder<?, ?, ?> builder =
         testForR8(parameters.getBackend())
-            .addProgramFiles(JAR)
+            .addInnerClassesAndStrippedOuter(getClass())
             .setMinApi(parameters)
-            .addKeepMainRule(MAIN);
+            .addKeepMainRule(Main.class);
     if (parameters.getBackend().isDex()) {
-      builder.run(parameters.getRuntime(), MAIN).assertSuccessWithOutputLines(EXPECTED);
+      builder.run(parameters.getRuntime(), Main.class).assertSuccessWithOutputLines(EXPECTED);
     } else {
       testForJvm(parameters)
           .addRunClasspathFiles(builder.compile().writeToZip())
-          .run(parameters.getRuntime(), MAIN)
+          .run(parameters.getRuntime(), Main.class)
           .assertSuccessWithOutputLines(EXPECTED);
+    }
+  }
+
+  public static final class Main {
+    public static void main(String[] args) {
+      Object obj = "Hello, world!";
+      if (obj instanceof String s) {
+        System.out.println(s);
+      }
     }
   }
 }
