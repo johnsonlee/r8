8dfed3572f0e4c789af8aa219d9939dbae96a116
Christoffer Adamsen <christofferqa@google.com>
2025-04-30 13:11:20 +0000

    Reproduce need for reflective identification from excluded classes
    Bug: b/414763615
    Change-Id: I7d080bdd4005d9c8c661ebb074cf38e26e8570b4

---
+++ src/test/java/com/android/tools/r8/partial/PartialCompilationReflectiveIdentificationTest.java
@@ -0,0 +1,48 @@
+// Copyright (c) 2025, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.partial;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class PartialCompilationReflectiveIdentificationTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void test() throws Exception {
+    parameters.assumeCanUseR8Partial();
+    testForR8Partial(parameters)
+        .addR8IncludedClasses(IncludedClass.class)
+        .addR8ExcludedClasses(ExcludedMain.class)
+        .compile()
+        .run(parameters.getRuntime(), ExcludedMain.class)
+        // TODO(b/414763615): Enable reflective identification for excluded classes.
+        .assertFailureWithErrorThatThrows(ClassNotFoundException.class);
+  }
+
+  static class ExcludedMain {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+      Class.forName(
+          "com.android.tools.r8.partial.PartialCompilationReflectiveIdentificationTest"
+              + "$IncludedClass");
+    }
+  }
+
+  static class IncludedClass {}
+}
