c8d18cdb3916baa8ad5b52c59705f02799f5ea23
Christoffer Adamsen <christofferqa@google.com>
2025-05-23 10:39:57 +0000

    Reproduce redundant bridge removal issue due to interface declarations
    Bug: b/419464490
    Change-Id: I459908798741695223a908d5e1745856302e8074

---
+++ src/test/java/com/android/tools/r8/bridgeremoval/B419464490Test.java
@@ -0,0 +1,93 @@
+// Copyright (c) 2025, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.bridgeremoval;
+
+import static com.android.tools.r8.utils.codeinspector.AssertUtils.assertFailsCompilationIf;
+
+import com.android.tools.r8.NeverClassInline;
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.NoHorizontalClassMerging;
+import com.android.tools.r8.NoVerticalClassMerging;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class B419464490Test extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void test() throws Exception {
+    // TODO(b/419464490): Should succeed with expected output.
+    assertFailsCompilationIf(
+        parameters.canUseDefaultAndStaticInterfaceMethods(),
+        () ->
+            testForR8(parameters)
+                .addInnerClasses(getClass())
+                .addKeepMainRule(Main.class)
+                .enableInliningAnnotations()
+                .enableNeverClassInliningAnnotations()
+                .enableNoHorizontalClassMergingAnnotations()
+                .enableNoVerticalClassMergingAnnotations()
+                .compile()
+                .run(parameters.getRuntime(), Main.class)
+                .assertSuccessWithOutputLines("I", "J"));
+  }
+
+  static class Main {
+
+    public static void main(String[] strArr) {
+      new A().m();
+      new A().callJ();
+    }
+  }
+
+  @NoHorizontalClassMerging
+  @NoVerticalClassMerging
+  interface I {
+
+    @NeverInline
+    default void m() {
+      System.out.println("I");
+    }
+  }
+
+  @NoHorizontalClassMerging
+  @NoVerticalClassMerging
+  interface J {
+
+    @NeverInline
+    default void m() {
+      System.out.println("J");
+    }
+  }
+
+  @NeverClassInline
+  static class A implements I, J {
+
+    @NeverInline
+    @Override
+    public void m() {
+      I.super.m();
+    }
+
+    // So that J.m is not removed by tree shaking.
+    @NeverInline
+    public void callJ() {
+      J.super.m();
+    }
+  }
+}
---
+++ src/test/java/com/android/tools/r8/bridgeremoval/IncorrectTargetAfterBridgeRemovalTest.java
@@ -0,0 +1,89 @@
+// Copyright (c) 2025, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.bridgeremoval;
+
+import com.android.tools.r8.NeverClassInline;
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.NoVerticalClassMerging;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class IncorrectTargetAfterBridgeRemovalTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void testJvm() throws Exception {
+    parameters.assumeJvmTestParameters();
+    testForJvm(parameters)
+        .addInnerClasses(getClass())
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputLines("A", "I");
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    testForR8(parameters)
+        .addInnerClasses(getClass())
+        .addKeepMainRule(Main.class)
+        .enableInliningAnnotations()
+        .enableNeverClassInliningAnnotations()
+        .enableNoVerticalClassMergingAnnotations()
+        .run(parameters.getRuntime(), Main.class)
+        .applyIf(
+            parameters.canUseDefaultAndStaticInterfaceMethods(),
+            // TODO(b/419464490): Should succeed with expected output.
+            rr -> rr.assertSuccessWithOutputLines("A", "A"),
+            rr -> rr.assertSuccessWithOutputLines("A", "I"));
+  }
+
+  static class Main {
+
+    public static void main(String[] args) {
+      new A().foo();
+      new B().foo();
+    }
+  }
+
+  @NoVerticalClassMerging
+  interface I {
+
+    @NeverInline
+    default void foo() {
+      System.out.println("I");
+    }
+  }
+
+  @NeverClassInline
+  @NoVerticalClassMerging
+  static class A {
+
+    @NeverInline
+    public void foo() {
+      System.out.println("A");
+    }
+  }
+
+  static class B extends A implements I {
+
+    @Override
+    @NeverInline
+    public void foo() {
+      I.super.foo();
+    }
+  }
+}
