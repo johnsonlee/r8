172e1dc3e6833d21802926dc7ac87116ae64035c
Christoffer Adamsen <christofferqa@google.com>
2025-05-26 09:16:00 +0000

    Reproduce invalid class merging in presence of default interface methods
    Bug: b/420228751
    Change-Id: Icf43e4a5caf80f3bf877506c4d56dbaa7fb49344

---
+++ src/test/java/com/android/tools/r8/classmerging/horizontal/B420228751AltTest.java
@@ -0,0 +1,98 @@
+// Copyright (c) 2025, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.classmerging.horizontal;
+
+import com.android.tools.r8.NeverClassInline;
+import com.android.tools.r8.NeverInline;
+import com.android.tools.r8.NeverPropagateValue;
+import com.android.tools.r8.NoMethodStaticizing;
+import com.android.tools.r8.NoVerticalClassMerging;
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class B420228751AltTest extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void testJvm() throws Exception {
+    parameters.assumeJvmTestParameters();
+    testForJvm(parameters)
+        .addInnerClasses(getClass())
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputLines("A", "I", "J");
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(getClass())
+        .addKeepMainRule(Main.class)
+        .addKeepClassAndMembersRules(I.class, J.class)
+        .enableInliningAnnotations()
+        .enableMemberValuePropagationAnnotations()
+        .enableNeverClassInliningAnnotations()
+        .enableNoMethodStaticizingAnnotations()
+        .enableNoVerticalClassMergingAnnotations()
+        .setMinApi(parameters)
+        .run(parameters.getRuntime(), Main.class)
+        // TODO(b/420228751): Disallow horizontal class merging.
+        .applyIf(
+            parameters.canUseDefaultAndStaticInterfaceMethods(),
+            rr -> rr.assertSuccessWithOutputLines("A", "J", "J"),
+            rr -> rr.assertSuccessWithOutputLines("A", "I", "J"));
+  }
+
+  static class Main {
+
+    public static void main(String[] strArr) {
+      System.out.println(new A().foo());
+      System.out.println(new B().m());
+      System.out.println(new C().m());
+    }
+  }
+
+  interface I {
+
+    default String m() {
+      return "I";
+    }
+  }
+
+  interface J extends I {
+
+    default String m() {
+      return "J";
+    }
+  }
+
+  @NeverClassInline
+  @NoVerticalClassMerging
+  static class A {
+
+    @NeverInline
+    @NeverPropagateValue
+    @NoMethodStaticizing
+    String foo() {
+      return "A";
+    }
+  }
+
+  static class B extends A implements I {}
+
+  static class C implements J {}
+}
---
+++ src/test/java/com/android/tools/r8/classmerging/horizontal/B420228751Test.java
@@ -0,0 +1,75 @@
+// Copyright (c) 2025, the R8 project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package com.android.tools.r8.classmerging.horizontal;
+
+import com.android.tools.r8.TestBase;
+import com.android.tools.r8.TestParameters;
+import com.android.tools.r8.TestParametersCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class B420228751Test extends TestBase {
+
+  @Parameter(0)
+  public TestParameters parameters;
+
+  @Parameters(name = "{0}")
+  public static TestParametersCollection data() {
+    return getTestParameters().withAllRuntimesAndApiLevels().build();
+  }
+
+  @Test
+  public void testJvm() throws Exception {
+    parameters.assumeJvmTestParameters();
+    testForJvm(parameters)
+        .addInnerClasses(getClass())
+        .run(parameters.getRuntime(), Main.class)
+        .assertSuccessWithOutputLines("I", "J");
+  }
+
+  @Test
+  public void testR8() throws Exception {
+    testForR8(parameters.getBackend())
+        .addInnerClasses(getClass())
+        .addKeepMainRule(Main.class)
+        .addKeepClassAndMembersRules(I.class, J.class)
+        .setMinApi(parameters)
+        .run(parameters.getRuntime(), Main.class)
+        // TODO(b/420228751): Disallow horizontal class merging.
+        .applyIf(
+            parameters.canUseDefaultAndStaticInterfaceMethods(),
+            rr -> rr.assertSuccessWithOutputLines("J", "J"),
+            rr -> rr.assertSuccessWithOutputLines("I", "J"));
+  }
+
+  static class Main {
+
+    public static void main(String[] strArr) {
+      System.out.println(new A().m());
+      System.out.println(new B().m());
+    }
+  }
+
+  interface I {
+
+    default String m() {
+      return "I";
+    }
+  }
+
+  interface J extends I {
+
+    default String m() {
+      return "J";
+    }
+  }
+
+  static class A implements I {}
+
+  static class B implements J {}
+}
