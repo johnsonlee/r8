3794fdc7e94008eb3b0e7a92bc441f3d95bd68ed
Christoffer Adamsen <christofferqa@google.com>
2025-09-03 14:58:12 +0200

    Version 9.0.7-dev
    Change-Id: I9c3810f45913a03d469cec4c6cde8adc8e1ca4da

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "9.0.6-dev";
+  public static final String LABEL = "9.0.7-dev";
 
   private Version() {
   }
