7a1fb91d6ccfc57f7fc1084057ddf034c82e7d8d
Jonathan Starup <jonathanlist@google.com>
2025-11-11 15:09:45 +0000

    refactored isMultiplication
    Change-Id: Idf8c9bf836bea1d22b200fce733b092e58817a4e

--- src/test/testbase/java/com/android/tools/r8/utils/codeinspector/CfInstructionSubject.java
+++ src/test/testbase/java/com/android/tools/r8/utils/codeinspector/CfInstructionSubject.java
@@ -382,11 +382,8 @@ public class CfInstructionSubject implements InstructionSubject {
 
   @Override
   public boolean isMultiplication() {
-    if (!(instruction instanceof CfArithmeticBinop)) {
-      return false;
-    }
-    int opcode = ((CfArithmeticBinop) instruction).getAsmOpcode();
-    return Opcodes.IMUL <= opcode && opcode <= Opcodes.DMUL;
+    return instruction instanceof CfArithmeticBinop
+        && ((CfArithmeticBinop) instruction).getOpcode() == CfArithmeticBinop.Opcode.Mul;
   }
 
   @Override
