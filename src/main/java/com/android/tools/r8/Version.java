e742de788bb8367f779a7be00850e7ff206bd77a
Christoffer Adamsen <christofferqa@google.com>
2025-11-28 08:42:30 +0100

    Version 9.0.28
    Change-Id: I06e0fada0a9b82b4a396c63c5392437a0f9e9e58

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "9.0.27";
+  public static final String LABEL = "9.0.28";
 
   private Version() {
   }
